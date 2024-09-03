c58203cfce72b4c953a7d18ec503bc890ae68db7
adityasinght <adityasingh.thakur@privacera.com>
2024-09-03 12:11:17 +0530

    Code cleanup for paig-client #34

--- paig-client/adhoc/sample1.py
+++
@@ -1,8 +0,0 @@
-import privacera_shield.client
-
-privacera_shield.client.setup(frameworks=["None"])
-with privacera_shield.client.create_shield_context(username="user1"):
-    try:
-        privacera_shield.client.dummy_access_denied()
-    except privacera_shield.exception.AccessControlException as e:
-        print(f"*** Access Denied, message: {e}")
--- paig-client/adhoc/t2.py
+++
@@ -1,5 +0,0 @@
-from langchain.llms.fake import FakeListLLM
-
-llm = FakeListLLM(responses=["a", "b", "c"])
-for i in range(10):
-    print(llm("prompt1"))
