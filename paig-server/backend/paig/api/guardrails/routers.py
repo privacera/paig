847344ca83ee036a573e6a4633224a7597892943
amrakshay <amrakshay@gmail.com>
2024-12-19 16:18:26 +0530

    Removed Sensitive Data API files

--- paig-server/backend/paig/api/guardrails/routers.py
+++ paig-server/backend/paig/api/guardrails/routers.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 
 from api.guardrails.routes.gr_connection_router import gr_connection_router
-from api.guardrails.routes.gr_sensitive_data_router import gr_sensitive_data_router
 from api.guardrails.routes.guardrail_router import guardrail_router
 from api.guardrails.routes.response_template_router import response_template_router
 
@@ -9,4 +8,3 @@ paig_guardrails_router = APIRouter()
 paig_guardrails_router.include_router(gr_connection_router, prefix="/connection", tags=["Guardrails connections"])
 paig_guardrails_router.include_router(guardrail_router, prefix="/guardrail", tags=["Guardrails"])
 paig_guardrails_router.include_router(response_template_router, prefix="/response_templates", tags=["Guardrails Response templates"])
-paig_guardrails_router.include_router(gr_sensitive_data_router, prefix="/sensitive_data", tags=["Guardrails Sensitive Data"])
