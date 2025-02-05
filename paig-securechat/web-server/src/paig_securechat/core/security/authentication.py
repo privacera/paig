0048a6bac306b203a827e2075700440ab566f156
Siddheshwar Kamble <siddheshwar.kamble@privacera.com>
2025-02-05 14:31:15 +0530

    Paig Securechat support api key and mysql db support

--- paig-securechat/web-server/src/paig_securechat/core/security/authentication.py
+++ paig-securechat/web-server/src/paig_securechat/core/security/authentication.py
@@ -4,7 +4,23 @@ from core.exceptions import UnauthorizedException
 from fastapi import Depends, Request
 from core.security.jwt import JWTHandler
 from core import constants
+from core import config
+import base64
+
 jwt_handler = JWTHandler()
+conf = config.load_config_file()
+
+
+
+def get_auth_user_and_token(api_key):
+    try:
+        decoded_key = base64.urlsafe_b64decode(api_key).decode()
+        username, api_token = decoded_key.split(":", 1)  # Extract username and token
+        return username, api_token
+    except Exception:
+        return None, None
+
+
 
 
 async def get_auth_user(
@@ -13,6 +29,16 @@ async def get_auth_user(
 ):
     if constants.SINGLE_USER_MODE:
         return await user_controller.get_user_by_user_name({"user_name": constants.DEFAULT_USER_NAME})
+    if "Authorization" in request.headers:
+        authorization = request.headers["Authorization"]
+        if "Bearer" not in authorization:
+            raise UnauthorizedException("Invalid authorization")
+        api_key = authorization.split("Bearer ")[1]
+        user_name, api_token = get_auth_user_and_token(api_key)
+        if api_token != conf["PAIG_SECURECHAT_API_TOKEN"]:
+            raise UnauthorizedException("Invalid authorization")
+        return await user_controller.get_user_by_user_name({"user_name": user_name})
+
     cookies = request.cookies
     session = cookies.get("session")
     if session is None:
