3f1d91c727734d1bc294c1b95c69d6b8773f2f0c
akhilesh <akhilesh.gupta@privacera.com>
2025-02-11 15:54:43 +0530

    Added changes for guardrail key decryption and response format

--- paig-server/backend/paig/api/shield/services/guardrail_service.py
+++ paig-server/backend/paig/api/shield/services/guardrail_service.py
@@ -134,12 +134,36 @@ async def get_guardrail_by_id(request, tenant_id):
     return response
 
 
+async def decrypted_connection_details(connection_details: dict):
+    from api.encryption.services.encryption_key_service import EncryptionKeyService
+    from api.encryption.api_schemas.encryption_key import EncryptionKeyView
+    from api.shield.utils.custom_exceptions import BadRequestException
 
-def test_guardrail(transformed_response:dict, message:str):
+    encryption_service = EncryptionKeyService()
+    encryption_key_info: EncryptionKeyView = await encryption_service.get_encryption_key_by_id(connection_details.get("encryption_key_id"))
 
-    final_result = {}
-    final_result.update(test_paig_guardrail(transformed_response, message))
-    final_result.update(test_aws_guardrail(transformed_response, message))
+    from paig_common.encryption import DataEncryptor
+    data_encryptor = DataEncryptor(public_key=encryption_key_info.public_key, private_key=encryption_key_info.private_key)
+    for key, value in connection_details.items():
+        connection_details_value = str(value)
+        if connection_details_value.startswith("GuardrailEncrypt:"):
+            try:
+                connection_details[key] = data_encryptor.decrypt(data=connection_details_value.replace("GuardrailEncrypt:", ""))
+            except Exception as e:
+                raise BadRequestException(
+                    f"Invalid connection details('{key}') for {connection_details_value}. Got error {e}")
+
+async def test_guardrail(transformed_response:dict, message:str):
+
+    final_result = [test_paig_guardrail(transformed_response, message)]
+    if isinstance(final_result[0], dict) and "DENY" not in final_result[0].get("action", ""):
+        test_aws_guardrail_result = await test_aws_guardrail(transformed_response, message)
+        if test_aws_guardrail_result:
+            final_result.append(test_aws_guardrail_result)
+
+    for result in final_result:
+        if result.get("action") == "DENY":
+            return result
     return final_result
 
 def test_paig_guardrail(transformed_response:dict, message:str) -> dict:
@@ -158,7 +182,7 @@ def test_paig_guardrail(transformed_response:dict, message:str) -> dict:
             redact_policies_dict.update({key: "<<"+key+">>"})
 
     if len(deny_policies_list) > 0:
-        return {"action": "DENY", "message": f'{transformed_response.get("config_type", {}).get("SENSITIVE_DATA", {}).get("response_message")}'}
+        return {"action": "DENY", "tags": scanner_result.get_traits(), "policy": ["SENSITIVE_DATA"], "message": f'{transformed_response.get("config_type", {}).get("SENSITIVE_DATA", {}).get("response_message")}'}
 
     if len(redact_policies_dict) > 0:
         # check the scanner_result for analyzer results
@@ -170,11 +194,11 @@ def test_paig_guardrail(transformed_response:dict, message:str) -> dict:
         anonymizer = PresidioAnonymizerEngine()
         masked_text = anonymizer.mask(message, redact_policies_dict, custom_mask_analyzer_result)
 
-        return {"action": "REDACT", "message": masked_text}
+        return {"action": "REDACT", "tags": scanner_result.get_traits(), "policy": ["SENSITIVE_DATA"], "message": masked_text}
 
-    return {"action": "ALLOW", "message": message}
+    return {"action": "ALLOW", "tags": scanner_result.get_traits(), "policy": ["SENSITIVE_DATA"], "message": message}
 
-def test_aws_guardrail(transformed_response:dict, message:str) -> dict:
+async def test_aws_guardrail(transformed_response:dict, message:str) -> None|dict:
 
     # check if AWS guardrail is present
     guardrail_provider_details = transformed_response.get("guardrail_provider_details", {})
@@ -183,22 +207,27 @@ def test_aws_guardrail(transformed_response:dict, message:str) -> dict:
         return {}
 
     aws_guardrail_details = guardrail_provider_details.get("AWS", {})
-    import api.shield.scanners.AWSBedrockGuardrailScanner as aws_guardrail_scanner
+    import api.shield.scanners.AWSBedrockGuardrailScanner as awsGuardrailScanner
     aws_guardrail_id = aws_guardrail_details.get("guardrailId")
     aws_guardrail_version = aws_guardrail_details.get("version")
     aws_guardrail_region = "us-east-1"
 
     aws_guardrail_connection_details = aws_guardrail_details.get("connection_details", {})
     if aws_guardrail_connection_details:
+        await decrypted_connection_details(aws_guardrail_connection_details)
         aws_guardrail_region = aws_guardrail_connection_details.get("region", "us-east-1")
 
-    aws_guardrail_scanner_instance = aws_guardrail_scanner.AWSBedrockGuardrailScanner(guardrail_id=aws_guardrail_id,
+    aws_guardrail_scanner_instance = awsGuardrailScanner.AWSBedrockGuardrailScanner(guardrail_id=aws_guardrail_id,
                                                                                       guardrail_version=aws_guardrail_version,
                                                                                       region=aws_guardrail_region,
                                                                                       connection_details=aws_guardrail_connection_details)
     scanner_result = aws_guardrail_scanner_instance.scan(message)
     if scanner_result.get("actions"):
-        #TODO: get response message based on policy identified from the guardrail
-        return {"action": "DENY", "Policy": scanner_result.get_traits(), "message": f'{aws_guardrail_scanner_instance.get_property("response_message")}'}
-    else:
-        return {}
+        policies_identified = []
+        response_msg = scanner_result.get("output_text")
+        for key, value in transformed_response.get("config_type", {}).items():
+            for category, action in value.get("configs", {}).items():
+                if category.upper() in scanner_result.get_traits():
+                    response_msg = value.get("response_message")
+                    policies_identified.append(key)
+        return {"action": "DENY", "tags": scanner_result.get_traits(), "policy": policies_identified, "message": response_msg }
