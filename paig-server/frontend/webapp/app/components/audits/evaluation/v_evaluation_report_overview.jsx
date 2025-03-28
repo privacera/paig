ac5de685a8c38b755fbfccde289fd4b74be802d4
Stalin Nadar <stalin.nadar@privacera.com>
2025-03-28 13:13:43 +0530

    paig_eval_phase2: Added pie chart and severity component fix

--- paig-server/frontend/webapp/app/components/audits/evaluation/v_evaluation_report_overview.jsx
+++ paig-server/frontend/webapp/app/components/audits/evaluation/v_evaluation_report_overview.jsx
@@ -1,7 +1,7 @@
 import React, {Component, Fragment} from "react";
 import {observer} from "mobx-react";
 
-import {Box, Grid, Paper, Typography} from "@material-ui/core";
+import {Box, Grid, Paper, Typography, Divider} from "@material-ui/core";
 
 import f from 'common-ui/utils/f';
 import RadialBarChart from "components/audits/evaluation/radial_bar_chart";
@@ -16,8 +16,8 @@ const moment = Utils.dateUtil.momentInstance();
 
 const SEVERITY_MAP = {
   "CRITICAL": {'label': 'Severe Failure', 'color': '#E101014D'},
-  "HIGH": {'label': 'High Conern', 'color': '#FFCAB3'},
-  "MEDIUM": {'label': 'Moderate Conern', 'color': '#FFEDB2'},
+  "HIGH": {'label': 'High Concern', 'color': '#FFCAB3'},
+  "MEDIUM": {'label': 'Moderate Concern', 'color': '#FFEDB2'},
   "LOW": {'label': 'Low Concern', 'color': '#B2F0D6'}
 }
 
@@ -45,35 +45,35 @@ const VEvalReportBasicInfo = ({model}) => {
     <PaperCard boxProps={{mb: 2}} paperProps={{'data-track-id': 'basic-report-info'}}>
       <Grid container spacing={3}>
         <Grid item md={4} sm={6} xs={12} className="border-right" alignItems="center">
-              <Box justifyContent='center'>
-              <Typography  variant="subtitle2" className="m-b-xs" >
-                Created
-              </Typography>
-              <Typography>
-              <EventIcon color="action" className="m-r-xs"/>
-              <span>{getCreateTime(model)}</span>
-              </Typography>
-              </Box>
+          <Box justifyContent='center'>
+          <Typography  variant="subtitle2" className="m-b-xs" >
+            Created
+          </Typography>
+          <Typography>
+          <EventIcon color="action" className="m-r-xs"/>
+          <span>{getCreateTime(model)}</span>
+          </Typography>
+          </Box>
         </Grid>
         <Grid item md={4} sm={6} xs={12} className="border-right" alignItems="center">
-              <div>
-              <Typography variant="subtitle2" className="m-b-xs">
-                Run By
-              </Typography>
-              <Typography>
-              <AccountCircleIcon color="action" className="m-r-xs"/>
-              <span>{model?.owner || '--'}</span>
-              </Typography>
-              </div>
+          <div>
+          <Typography variant="subtitle2" className="m-b-xs">
+            Run By
+          </Typography>
+          <Typography>
+          <AccountCircleIcon color="action" className="m-r-xs"/>
+          <span>{model?.owner || '--'}</span>
+          </Typography>
+          </div>
         </Grid>
         <Grid item md={4} sm={6} xs={12}>
-              <Typography variant="subtitle2" className="m-b-xs">
-                Run As
-              </Typography>
-              <Typography>
-              <ContactsIcon color="action" className="m-r-xs"/>
-              <span>{getRunAs(model)}</span>
-              </Typography>
+          <Typography variant="subtitle2" className="m-b-xs">
+            Run As
+          </Typography>
+          <Typography>
+          <ContactsIcon color="action" className="m-r-xs"/>
+          <span>{getRunAs(model)}</span>
+          </Typography>
         </Grid>
       </Grid>
     </PaperCard>
@@ -86,53 +86,92 @@ class VEvaluationReportOverview extends Component {
   render() {
     const { _vState, parent_vState, cEvaluationOverview, data, handlePageChange, handleSearchByField } = this.props;
     const evaluationDataList = f.models(cEvaluationOverview) || [];
+    const reportStats = f.models(_vState?.reportStats) || [];
     return (
       <Fragment>
         <VEvalReportBasicInfo model={_vState.reportData}/>
         <Grid container spacing={3}>
-            <Grid item spacing={3} md={5} sm={5} xs={12}>
-                  <PaperCard boxProps={{ mb: 2 }}>
-                    <Loader
-                      promiseData={cEvaluationOverview}
-                      loaderContent={getSkeleton("THREE_SLIM_LOADER")}
-                    >
-                      <Grid container spacing={2}>
-                        {evaluationDataList.map((evaluationData, index) => {
-                          return (
-                            <Grid item md={4} sm={4} xs={12} key={index} className="graph-border-left m-l-md">
-                              <Typography className="graph-title" gutterBottom>
-                                Overall Score
-                              </Typography>
-                              <RadialBarChart
-                                chartData={evaluationData}
-                              />
-                            </Grid>
-                          );
-                        })}
-                      </Grid>
+          <Grid item md={4} sm={5} xs={12}>
+            <PaperCard>
+              <Loader
+                promiseData={cEvaluationOverview}
+                loaderContent={getSkeleton("THREE_SLIM_LOADER")}
+              >
+                <Grid container spacing={2}>
+                  {evaluationDataList.map((evaluationData, index) => {
+                    return (
+                      <Fragment>
+                        <Grid item md={12} sm={12}>
+                          <Typography className="graph-title inline-block" gutterBottom>
+                            Overall Score
+                          </Typography>
+                        </Grid>
+                        <RadialBarChart
+                          chartData={evaluationData}
+                        />
+                      </Fragment>
+                    );
+                  })}
+                </Grid>
 
-                    </Loader>
-                  </PaperCard>
-            </Grid>
-            <Grid item md={7} sm={12}>
-              <Grid container spacing={3}>
-                  {Object.entries(SEVERITY_MAP).map(([severityKey, data], index) => (
-                    <Grid item md={6} sm={12} key={index}>
-                      <Paper data-track-id="top-users">
-                        {/* Header */}
-                          <Box sx={{ backgroundColor: data.color, p: 1}}>
-                            <Typography variant="bod1">
-                              {data.label}
+              </Loader>
+            </PaperCard>
+          </Grid>
+          <Grid item md={8} sm={12} className="eval-metrics-container">
+            <Grid container spacing={3} className="eval-metrics-box">
+              {Object.entries(SEVERITY_MAP).map(([severityKey, data], index) => (
+                <Grid item md={6} sm={12} key={index} className="eval-metrics">
+                  <Paper data-track-id="top-users">
+                    {/* Header */}
+                      <Box sx={{ backgroundColor: data.color, p: 1}}>
+                        <Typography variant="subtitle1" className="graph-title">
+                          {data.label}
+                        </Typography>
+                      </Box>
+
+                      {/* Content area */}
+                      <Box display="flex" p={2} style={{gap:'10px'}}>
+                        {Object.entries(_vState?.reportSeverity || {}).map(([applicationName, severities], idx, array) => (
+                          <>
+                          <Box
+                            key={applicationName}
+                            flex={1}
+                            justifyContent="space-between"
+                            alignItems="center"
+                            mb={1}
+                          >
+                          <Typography variant="subtitle1" color="textSecondary">
+                              {applicationName}
+                            </Typography>
+                            <Typography variant="body1" color="textPrimary" className="graph-title">
+                              {severities?.[severityKey] || 0}
                             </Typography>
                           </Box>
-                          {/* Content area */}
-                          <Box display="flex" justifyContent="space-between" alignItems="center" p={2}>
-                          </Box>
-                      </Paper>
-                    </Grid>
-                  ))}
+                          {idx < array.length - 1 && (
+                            <Divider orientation="vertical" flexItem />
+                          )}
+                          </>
+                        ))}
+                      </Box>
+                  </Paper>
                 </Grid>
+              ))}
             </Grid>
+          </Grid>
+        </Grid>
+        <Grid container spacing={3}>  
+          {reportStats.map((stat, index) => (
+            <Grid item md={6} sm={12} xs={12} key={index}>
+              <PaperCard boxProps={{ mb: 2 }}>
+                {/* <Typography variant="h6" color="textPrimary">
+                  {stat.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  {stat.value || 0}
+                </Typography> */}
+              </PaperCard>
+            </Grid>
+          ))}
         </Grid>
       </Fragment>
     );
