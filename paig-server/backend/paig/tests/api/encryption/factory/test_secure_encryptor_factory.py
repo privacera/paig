086c0755689b249e1acc7a389ed801f7f8b32e84
Siddheshwar Kamble <siddheshwar.kamble@privacera.com>
2025-04-11 16:28:56 +0530

    Updated secure encryptor factory class

--- paig-server/backend/paig/tests/api/encryption/factory/test_secure_encryptor_factory.py
+++ paig-server/backend/paig/tests/api/encryption/factory/test_secure_encryptor_factory.py
@@ -5,26 +5,22 @@ from api.encryption.database.db_models.encryption_master_key_model import Encryp
 from api.encryption.database.db_operations.encryption_master_key_repository import EncryptionMasterKeyRepository
 from api.encryption.utils.secure_encryptor import SecureEncryptor
 from api.encryption.factory.secure_encryptor_factory import SecureEncryptorFactory
+from api.encryption.events.startup import create_encryption_master_key_if_not_exists
 
 
 @pytest.fixture
-def mock_encryption_master_key_repository():
-    return AsyncMock(spec=EncryptionMasterKeyRepository)
+def encryption_master_key_repository():
+    return EncryptionMasterKeyRepository()
 
 
 @pytest.mark.asyncio
-async def test_get_secure_encryptor(mock_encryption_master_key_repository):
-    # Create a mock encryption master key model
-    mock_master_key = EncryptionMasterKeyModel(id=1, key="test_master_key")
+async def test_get_secure_encryptor(encryption_master_key_repository, db_session, set_context_session):
+    await create_encryption_master_key_if_not_exists()
 
-    # Configure the mock repository to return the mock master key
-    mock_encryption_master_key_repository.get_active_encryption_master_key.return_value = mock_master_key
-
-    secure_encryptor_factory = SecureEncryptorFactory(mock_encryption_master_key_repository)
+    secure_encryptor_factory = SecureEncryptorFactory(encryption_master_key_repository)
 
     # Call the method to be tested
     secure_encryptor = await secure_encryptor_factory.get_or_create_secure_encryptor()
 
     # Assertions
     assert isinstance(secure_encryptor, SecureEncryptor)
-    mock_encryption_master_key_repository.get_active_encryption_master_key.assert_called_once()
